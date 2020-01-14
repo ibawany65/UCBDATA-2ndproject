4e60603a549edd5f6fc7d2b8dfd55a0c54b62f61
Bryan Lopez <bryan.lopez@berkeley.edu>
2020-01-13 23:39:19 -0800

    Added Control to select on/off Layers of zip codes
    Bug #1: Not sorted Zip Codes. Currently bugged where sorting and sorting function do not allow for control to render

--- static/js/page.js
+++ static/js/page.js
@@ -1,3 +1,12 @@
+function compLayer(first, second) {
+  // Sorts Layers by Name
+  let a = Number(first.feature.properties.zip);
+  let b = Number(second.feature.properties.zip);
+  console.log(a);
+  console.log(b);
+  return a-b;
+}
+
 function makePage() {
 
   // Chicago Lat, Long
@@ -5,24 +14,31 @@ function makePage() {
   const long = -87.6298;
 
   // Make Map
-  var map = L.map("map_id").setView([lat, long], 13);
+  var map = L.map("map_id", {
+    zoomSnap: .5,
+  }).setView([lat, long], 10.5);
+
+  var layersControl = L.control.layers().addTo(map);
 
   // Street Title Layer
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    maxZoom: 13,
+    id: 'mapbox/streets-v10',
     accessToken: APIKEY
 }).addTo(map);
 
-  d3.json("/static/resources/zip_boundaries.geojson", function(data) {
-    L.geoJSON(data.features, {
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup(`ZIP: ${feature.properties.zip}`);
-      }
-    }).addTo(map);
-  });
+// ZIP Layering
+d3.json("/static/resources/zip_boundaries.geojson", function(data) {
+  L.geoJSON(data.features, {
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(`ZIP: ${feature.properties.zip}`);
+      layersControl.addOverlay(layer, `${feature.properties.zip}`);
+    }
+  }).addTo(map);
+});
 
+  // Control
 
   // makePage END
 };
